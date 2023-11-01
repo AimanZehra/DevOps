3bf406102523dc8ed8c52a922fe03efccff38fc3
Aiman Zehra <aiman.-@stud.fra-uas.de>
2023-11-02 05:02:30 +0530

    added readme file

--- Shell Script/example 1.sh
+++ Shell Script/example 1.sh
@@ -4,15 +4,20 @@
 # Author: Aiman  Zehra
 # Date: 01.11.2023
 #
-# Shell script to print the numbers 
-# divisible by 3 and 5  and not 15
+# Practice Examples 
 #
 # Version: 1.0 (v1)
 ######################################
 
+# Shell script to print the numbers divisible by 3 and 5  and not 15
 for i in {1..100}; do
 if (['expr' $i % 3 == 0] || ['expr' $i % 5 == 0]) && ['expr' $i % 15 != 0]
 then 
     echo $i
 fi;
 done
+
+# Shell script to write the number of 's' in mississipi
+
+x = mississipi
+grep -o "s" <<< "$x" | wc -l
---
+++ Shell Script/readme.md
@@ -0,0 +1,14 @@
+## Shell Scripting
+
+**Difference between soft and hard link?**
+
+
+**Disadvantages of Shell scripting?**
+
+* Errors are frequest and costly, and a single error can alter the command
+* The execution sped is slow.
+* Bugs or inadequacies in the language's syntax or implementation.
+* Large, complex tasks aren't well suited to it.
+* Contrary to other scripting languages, etc. it provides a minimal data structure.
+* Every time a shell command is executed, a new process is launched.
+
