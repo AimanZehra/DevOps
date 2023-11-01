9bd65dd9409f77a546bcdc010191b2b0008451ad
Aiman Zehra <aiman.-@stud.fra-uas.de>
2023-11-01 20:53:10 +0530

    commit

--- Shell Script/nodeHealth.sh
+++ Shell Script/nodeHealth.sh
@@ -11,7 +11,10 @@
 
 set -x # Debug mode
 set -e # Exit the script when there is an error
-set -o # Pipefail
+set -o pipefail
+
+# We can also write the above commands as:
+set - exo pipefail
 
 # To print the disk space
 df -h
@@ -30,3 +33,12 @@ ps -ef | grep Amazon
 
 # To fetch any specific column we use 'awk' eg:-
 ps -ef | grep amazon | awk -F" " '{print $2}'
+
+# To retrieve the information from the internet we use 'curl' eg:- 
+curl https://github.com/AimanZehra/DevOps/tree/main/Terraform | grep error
+
+# To get details of any command to use, simply use 'man' before it eg:-
+man curl
+
+# To get the details of any logfile/any external link and get it 'DOwnloaded' we use 'wget' eg:-
+wget https://github.com/AimanZehra/DevOps/tree/main/Terraform
